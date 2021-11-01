65b032f6763acb5d48d7681890c94b22cd09f644
Philipp Rieser <philipp.rieser@gmail.com>
2021-11-01 21:53:09 +0100

    spinner

--- js/google.js
+++ js/google.js
@@ -1,7 +1,11 @@
 // Form to Google Sheets
 const form = document.forms['google-sheet'];
+const spinner = document.getElementById('spinner');
+
 form.addEventListener('submit', (e) => {
   e.preventDefault();
+  spinner.removeAttribute('hidden');
+
   fetch(
     'https://script.google.com/macros/s/AKfycbxS9Ddcv6P5Pk6KqlvMaC1CaDnqagwmC46YjdRwM2IO2Zr6gJSG4DNu7f9xQnie9chz9g/exec',
     {
@@ -9,6 +13,6 @@ form.addEventListener('submit', (e) => {
       body: new FormData(form),
     }
   )
-    .then((response) => alert('You have successfully submitted.'))
+    .then(() => spinner.setAttribute('hidden', ''))
     .catch((error) => console.error('Error!', error.message));
 });
